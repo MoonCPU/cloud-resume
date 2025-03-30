bcdd5241578c40687251f376ab71052dfdafd9e5
Moon <moonkang20@gmail.com>
2025-03-30 20:15:48 -0300

    fixing s3 terraform

--- frontend/terraform/modules/s3/main.tf
+++ frontend/terraform/modules/s3/main.tf
@@ -2,15 +2,15 @@ module "frontend_s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = "moon-cloud-ops-s3"
-  acl    = "public-read"
 
   versioning = {
     enabled = true
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "allow_public" {
+resource "aws_s3_bucket_public_access_block" "force_open" {
   bucket = module.frontend_s3_bucket.s3_bucket_id
+  
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
@@ -34,50 +34,44 @@ resource "aws_s3_bucket_website_configuration" "frontend_website" {
 resource "aws_s3_object" "index_html" {
   bucket       = module.frontend_s3_bucket.s3_bucket_id
   key          = "index.html"
-  source       = "../../../app/index.html"
+  source       = "../app/index.html"
   content_type = "text/html"
-  acl          = "public-read"
 }
 
 resource "aws_s3_object" "style_css" {
   bucket       = module.frontend_s3_bucket.s3_bucket_id
   key          = "style.css"
-  source       = "../../../app/style.css"
+  source       = "../app/style.css"
   content_type = "text/css"
-  acl          = "public-read"
 }
 
 resource "aws_s3_object" "main_js" {
   bucket       = module.frontend_s3_bucket.s3_bucket_id
   key          = "main.js"
-  source       = "../../../app/main.js"
+  source       = "../app/main.js"
   content_type = "application/javascript"
-  acl          = "public-read"
 }
 
 //uploading the images to s3 bucket 
 resource "aws_s3_object" "github_logo" {
   bucket       = module.frontend_s3_bucket.s3_bucket_id
   key          = "images/github.png" # Maintain folder structure in S3
-  source       = "../../../app/images/github.png"
+  source       = "../app/images/github.png"
   content_type = "image/png"
-  acl          = "public-read"
 }
 
 resource "aws_s3_object" "linkedin_logo" {
   bucket       = module.frontend_s3_bucket.s3_bucket_id
   key          = "images/linkedin.png"
-  source       = "../../../app/images/linkedin.png"
+  source       = "../app/images/linkedin.png"
   content_type = "image/png"
-  acl          = "public-read"
 }
 
 resource "aws_s3_object" "kilroy_logo" {
   bucket       = module.frontend_s3_bucket.s3_bucket_id
   key          = "images/kilroy.png"
-  source       = "../../../app/images/kilroy.png"
+  source       = "../app/images/kilroy.png"
   content_type = "image/png"
-  acl          = "public-read"
 }
 
 //setting up policy and permissions
@@ -91,21 +85,6 @@ data "aws_iam_policy_document" "frontend_policy" {
       identifiers = ["*"]
     }
   }
-
-  #   statement {
-  #   effect    = "Deny"
-  #   actions   = ["s3:*"]  # Applies to all S3 actions
-  #   resources = ["${module.frontend_s3_bucket.s3_bucket_arn}/*"]
-  #   principals {
-  #     type        = "*"
-  #     identifiers = ["*"]
-  #   }
-  #   condition {
-  #     test     = "Bool"
-  #     variable = "aws:SecureTransport"
-  #     values   = ["false"]  # Blocks HTTP requests
-  #   }
-  # }
 
   statement {
     effect    = "Deny"
@@ -121,4 +100,8 @@ data "aws_iam_policy_document" "frontend_policy" {
 resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
   bucket = module.frontend_s3_bucket.s3_bucket_id
   policy = data.aws_iam_policy_document.frontend_policy.json
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.force_open
+  ]
 }
